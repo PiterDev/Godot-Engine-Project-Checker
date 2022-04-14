63fd21573707c3fc880af185c40caaaa75e419b4
Piter <61157319+PiterDev@users.noreply.github.com>
2022-04-14 17:39:09 +0200

    Add script

---
+++ godot_checker.py
@@ -0,0 +1,25 @@
+import os
+
+
+def look_for_errors(text: str, filename: str):
+	if filename.lower() != filename:
+		print(f"Filename is not lowercase: {filename}")
+	for i, line in enumerate(text.split('\n')):
+		if line.startswith('#'):
+			continue
+		if line.startswith("var"):
+			if not ":" in line:
+				print(f"[{filename}] Line {i + 1}: variable might not be statically typed: {line}")
+
+
+def scan_directory(directory):
+	for root, dirs, files in os.walk(directory):
+		for file in files:
+			if file.endswith(".gd"):
+				with open(os.path.join(root, file), 'r', encoding="UTF-8") as f:
+					look_for_errors(f.read(), file)
+			if not file.islower():
+				print(f"Filename is not lowercase: {file}")
+
+
+scan_directory("./")
